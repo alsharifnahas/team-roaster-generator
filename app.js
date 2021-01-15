576dcab2b11e78ddeb2f4be82097609abc82ad1b
Alsharif Nahas <eid_alshareef@hotmail.com>
2021-01-14 22:12:38 -0500

    added the output folder. fixed bugs

--- app.js
+++ app.js
@@ -1,89 +1,82 @@
+// importing modules
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
 const fs = require("fs");
 const render = require("./lib/htmlRenderer");
+const path = require("path");
 
-// Variable to store employee information
+
+const OUTPUT_DIR = path.resolve(__dirname, "./output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+// variable to store employee information
 const employees = [];
 
-// Function to ask if the user wants to add a new member
-const addMember = () => {
+
+// function to ask if the user wants to add a new member
+const addEmployee = () => {
     inquirer
         .prompt([
             {
                 type: "list",
                 message: "Would you like to add a new member to your team?",
-                name: "addNewMember",
-                choices: ["Engineer", "Intern", "No new members"],
+                name: "newMember",
+                choices: ["Engineer", "Intern", "I don't want to add"],
             },
         ])
         .then((response) => {
             // Conditional to check which team member the user chose, if any
-            if (response.addNewMember === "Engineer") {
-                getEngineerInfo();
-            } else if (response.addNewMember === "Intern") {
-                getInternInfo();
-            } else {
-                console.log("Your team members have been added!");
-                // If an output directory exists, render employees
-                if (fs.existsSync("./output")) {
-                    const renderEmployees = render(employees);
-                    fs.writeFileSync(outputPath, renderEmployees, "utf-8");
-                } else {
-                    // If an output directory does not exist, create one and render employees
-                    fs.mkdir("./output", function (err) {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log("New directory successfully created.");
-                            const renderEmployees = render(employees);
-                            fs.writeFileSync(outputPath, renderEmployees, "utf-8");
-                        }
-                    });
-                }
+
+            switch (response.newMember) {
+                case "Engineer":
+                    getEngineerInfo();
+                    break;
+                case "Intern":
+                    getInternInfo();
+                    break;
+                case "I don't want to add":
+                    renderHTML();
             }
         });
 };
 
-// Function to get manager information (name, ID, email, and office number)
+// function to get manager's information
 const getManagerInfo = () => {
     inquirer
         .prompt([
             {
                 type: "input",
                 message: "What is your manager's name?",
-                name: "managerName",
+                name: "name",
             },
             {
                 type: "input",
                 message: "What is your manager's ID?",
-                name: "managerID",
+                name: "id",
             },
             {
                 type: "input",
                 message: "What is your manager's email?",
-                name: "managerEmail",
+                name: "email",
             },
             {
                 type: "input",
                 message: "What is your manager's office number?",
-                name: "managerOfficeNumber",
+                name: "officeNumber",
             },
         ])
         .then((response) => {
-            // Ask if the user wants to add a member, then render employees
-            addMember();
-            // Create a new manager with the user's responses
+
+
             const manager = new Manager(
-                response.managerName,
-                response.managerID,
-                response.managerEmail,
-                response.managerOfficeNumber
+                response.name,
+                response.id,
+                response.email,
+                response.officeNumber
             );
-            // Put the new manager into the employees array
+            addEmployee();
+
             employees.push(manager);
         });
 };
@@ -95,34 +88,35 @@ const getEngineerInfo = () => {
             {
                 type: "input",
                 message: "What is your engineer's name?",
-                name: "engineerName",
+                name: "name",
             },
             {
                 type: "input",
                 message: "What is your engineer's ID?",
-                name: "engineerID",
+                name: "id",
             },
             {
                 type: "input",
                 message: "What is your engineer's email?",
-                name: "engineerEmail",
+                name: "email",
             },
             {
                 type: "input",
                 message: "What is your engineer's GitHub username?",
-                name: "engineerGitHub",
+                name: "gitHub",
             },
         ])
         .then((response) => {
             // Ask if the user wants to add a member, then render employees
-            addMember();
+
             // Create a new engineer with the user's responses
             const engineer = new Engineer(
-                response.engineerName,
-                response.engineerID,
-                response.engineerEmail,
-                response.engineerGitHub
+                response.name,
+                response.id,
+                response.email,
+                response.gitHub
             );
+            addEmployee();
             // Put the new engineer into the employees array
             employees.push(engineer);
         });
@@ -135,38 +129,49 @@ const getInternInfo = () => {
             {
                 type: "input",
                 message: "What is your intern's name?",
-                name: "internName",
+                name: "name",
             },
             {
                 type: "input",
                 message: "What is your intern's ID?",
-                name: "internID",
+                name: "id",
             },
             {
                 type: "input",
                 message: "What is your intern's email?",
-                name: "internEmail",
+                name: "email",
             },
             {
                 type: "input",
                 message: "Where does your intern go to school?",
-                name: "internSchool",
+                name: "school",
             },
         ])
         .then((response) => {
-            // Ask if the user wants to add a member, then render employees
-            addMember();
+
             // Create a new intern with the user's responses
             const intern = new Intern(
-                response.internName,
-                response.internID,
-                response.internEmail,
-                response.internSchool
+                response.name,
+                response.id,
+                response.email,
+                response.school
             );
+            addEmployee();
             // Put the new intern into the employees array
             employees.push(intern);
         });
 };
+
+const renderHTML = () => {
+    console.log("The HTML team page has been generated");
+    const htmlPage = render(employees);
+    fs.writeFile(outputPath, htmlPage, (err) =>
+        err
+            ? console.error(err)
+            : console.log("file created successfully")
+    );
+
+}
 
 // Function to initialize app
 const init = () => {
